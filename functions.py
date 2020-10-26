17a849c10cc035636164268d5a5d7514bdc67833
lizette98 <andreal98.acg@gmail.com>
2020-10-26 12:17:41 -0600

    MAD

--- functions.py
+++ functions.py
@@ -31,7 +31,9 @@ def f_leer_archivo(param_archivo):
     param_archivo: DataFrame
             Dataframe de historial con informacion del trading
     """
+    # Quitar el "-e" de la columna Item
     param_archivo['Item'] = param_archivo['Item'].map(lambda x: str(x)[:-2])
+    # Poner los activos en minuscula
     param_archivo['Item'] = param_archivo['Item'].str.lower()
 
     return param_archivo
@@ -51,9 +53,11 @@ def f_pip_size(param_ins):
     n: int
             Multiplicador de pips que le corresponde al instrumento.
     """
+    # Leer el archivo de Oanda con la informacion de pips
     pips_oanda = dt.pips_oanda
     pips_oanda = pips_oanda.set_index('Item')
     pips_oanda = pips_oanda['PipLocation']
+    # Formula para obtener el multiplicador de cada activo
     n = pow(10 / 1, np.abs(pips_oanda[param_ins]))
 
     return n
@@ -73,14 +77,18 @@ def f_columnas_tiempos(param_data):
     param_data: DataFrame
             Dataframe inicial, ahora con la columna del tiempo que duro la transaccion en segundos.
     """
-    param_data = dt.archivo
+    # Quitar espacios de columna de Profit
     param_data['Profit'] = param_data['Profit'].str.replace(' ', '')
+    # Convertir a datetime las columnas de tiempo
     param_data['Close Time'] = pd.to_datetime(param_data['Close Time'])
     param_data['Open Time'] = pd.to_datetime(param_data['Open Time'])
+    # Renombrar las columnas de tiempo
     param_data = param_data.rename(columns={'Close Time': 'CloseTime', 'Open Time': 'OpenTime'})
     #Nueva columna de tiempo transcurrido en segundos
     param_data['tiempo'] = (param_data['CloseTime'] - param_data['OpenTime']).dt.seconds
+    #Renombrar las columnas de precio de apertura y de cierre
     param_data = param_data.rename(columns={'Price': 'OpenPrice', 'Price.1': 'ClosePrice'})
+
     return param_data
 
 
@@ -99,7 +107,9 @@ def f_columnas_pips(param_data):
             Dataframe anterior, ahora con las columnas de pips resultantes de cada operacion,
             valor acumulado de pips y valor acumulado de la columna profit.
     """
+    #Inicializa columna de pips
     param_data['pips'] = 0
+    # Ciclo para obtener los pips de cada operacion
     for i in range(len(param_data)):
         n = f_pip_size(param_data['Item'].iloc[i])
         if param_data['Type'][i] == 'sell':
@@ -107,8 +117,11 @@ def f_columnas_pips(param_data):
 
         else:
             param_data['pips'][i] = (param_data['ClosePrice'][i] - param_data['OpenPrice'][i]) * n
+    # Columna de pips acumulados
     param_data['pips_acm'] = param_data['pips'].cumsum()
+    #Convertir columna de Profit a int
     param_data['Profit'] = pd.to_numeric(param_data['Profit'])
+    #Columna de Profit acumulada
     param_data['profit_acm'] = param_data['Profit'].cumsum()
     return param_data
 
@@ -133,7 +146,7 @@ def f_estadisticas_ba(param_data):
     df_1_ranking:
             Dataframe con el instrumento y el ratio de efectividad de las operaciones realizadas.
     """
-
+    # Creacion del primer df del diccionario
     df_1_tabla = pd.DataFrame({'Ops totales': [len(param_data['Ticket']), 'Operaciones totales'],
                                'Ganadoras': [len(param_data[param_data['Profit'] >= 0]), 'Operaciones ganadoras'],
                                'Ganadoras_c': [len(param_data[(param_data['Type'] == 'buy') & (param_data['Profit'] >= 0)]),
@@ -161,6 +174,7 @@ def f_estadisticas_ba(param_data):
                         for i in param_data.Item.unique()}, index=['rank']).transpose()
 
     convert_dict = {'valor': float}
+    # Convertir a diccionario
     df_1_tabla = df_1_tabla.astype(convert_dict)
 
     df_1_ranking = (tb1 * 100).sort_values(by='rank', ascending=False).T.transpose()
@@ -187,14 +201,14 @@ def f_profit_acm_d(param_data):
             y se suma las ganancias o perdidas de la columna 'profit_acm'.
     """
 
-    # Se forma una nueva columna inicializada en $100,000 donde se le suma/resta el profit acumulado
+    # Columna inicializada en $100,000 donde se le suma/resta el profit acumulado
     param_data['profit_acm_d'] = 100000 + param_data.profit_acm
 
     return param_data.copy()
 
 def f_evolucion_capital(param_data):
     """
-    Funcion para agregar mas columnas de transformaciones de tiempo.
+    Funcion para analizar la evolucion del capital.
 
     Parameters
     ----------
@@ -210,24 +224,24 @@ def f_evolucion_capital(param_data):
             3. profit_acm_d: profit acumulado diario de la cuenta de capital.
     """
 
-    # Agregar normalize a closetime
+    # Agregar normalize a Closetime
     diario = pd.date_range(param_data.CloseTime.min(), param_data.CloseTime.max()).normalize()
 
-    # convertir a dataframe las fechas diarias
+    # Convertir a df las fechas diarias
     fechas = pd.DataFrame({'timestamp': diario})
 
     # Agregar normalize a groupby
     groups = param_data.groupby(pd.DatetimeIndex(param_data['CloseTime']).normalize())
 
     profit = groups['Profit'].sum()
-    # convertir los profits diarios a dataframe
+    # Convertir los profits diarios a df
     profit_diario = pd.DataFrame({'profit_d': [profit[i] if i in profit.index else 0 for i in diario]})
     profit_acm = np.cumsum(profit_diario) + 100000
-    # juntar en un solo dataframe los dos dataframes anteriores fechas y profits diarios
+    # Juntar en un solo df los dos df anteriores de fechas y profit diario
     f_p1 = pd.merge(fechas, profit_diario, left_index=True, right_index=True)
-    # juntar el dataframe anterior de los dos df con los profits acumulados
+    # Juntar el df anterior con los profit acumulados
     df_profit_diario1 = pd.merge(f_p1, profit_acm, left_index=True, right_index=True)
-    # renombrar las columnas del nuevo dataframe
+    # Renombrar las columnas del df resultante
     df_profit_diario = df_profit_diario1.rename(columns={"profit_d_x": "profit_d", "profit_d_y": "profit_acm_d"})
 
     return df_profit_diario
@@ -260,36 +274,37 @@ def f_estadisticas_mad(param_data):
     rf = 0.05 / 300
     sdp = np.std(rp)
 
-    # Drawdown Capital
-    where_row = profit_dia.loc[profit_dia['profit_acm_d'] == profit_dia.profit_acm_d.min()]
-    where_position = where_row.index.tolist()
+    # DrawDown Capital
+    fila = profit_dia.loc[profit_dia['profit_acm_d'] == profit_dia.profit_acm_d.min()]
+    position = fila.index.tolist()
 
-    prev_where = profit_dia.loc[0:where_position[0]]
-    where_max_prev = profit_dia.loc[profit_dia['profit_acm_d'] == prev_where.profit_acm_d.max()]
-    where_min_prev = profit_dia.loc[profit_dia['profit_acm_d'] == prev_where.profit_acm_d.min()]
-    max_ddown = where_max_prev.iloc[0]['profit_acm_d']
-    min_ddown = where_min_prev.iloc[0]['profit_acm_d']
-    ddown = max_ddown - min_ddown
+    prev_where = profit_dia.loc[0:position[0]]
+    max_prev = profit_dia.loc[profit_dia['profit_acm_d'] == prev_where.profit_acm_d.max()]
+    min_prev = profit_dia.loc[profit_dia['profit_acm_d'] == prev_where.profit_acm_d.min()]
+    ddown_max = max_prev.iloc[0]['profit_acm_d']
+    ddown_min = min_prev.iloc[0]['profit_acm_d']
+    ddown = ddown_max - ddown_min
 
-    fecha_i_ddown = where_max_prev.iloc[0]['timestamp']
-    fecha_f_ddown = where_min_prev.iloc[0]['timestamp']
-    drawdown = "{}, {}, ${:.2f}".format(fecha_i_ddown, fecha_f_ddown, ddown)
+    date_max_ddown = max_prev.iloc[0]['timestamp']
+    date_min_ddown = min_prev.iloc[0]['timestamp']
+    drawdown = "{}, {}, ${:.2f}".format(date_max_ddown, date_min_ddown, ddown)
 
     # DrawUp Capital
 
-    where_row_up = profit_dia.loc[profit_dia['profit_acm_d'] == profit_dia.profit_acm_d.max()]
-    where_position_up = where_row_up.index.tolist()
+    fila_up = profit_dia.loc[profit_dia['profit_acm_d'] == profit_dia.profit_acm_d.max()]
+    position_up = fila_up.index.tolist()
 
-    foll_where = profit_dia.loc[0:where_position_up[0]]
-    where_max_foll = profit_dia.loc[profit_dia['profit_acm_d'] == foll_where.profit_acm_d.max()]
-    where_min_foll = profit_dia.loc[profit_dia['profit_acm_d'] == foll_where.profit_acm_d.min()]
-    max_dup = where_max_foll.iloc[0]['profit_acm_d']
-    min_dup = where_min_foll.iloc[0]['profit_acm_d']
-    dup = max_dup - min_dup
+    foll_where = profit_dia.loc[0:position_up[0]]
+    max_foll = profit_dia.loc[profit_dia['profit_acm_d'] == foll_where.profit_acm_d.max()]
+    min_foll = profit_dia.loc[profit_dia['profit_acm_d'] == foll_where.profit_acm_d.min()]
+    dup_max = max_foll.iloc[0]['profit_acm_d']
+    dup_min = min_foll.iloc[0]['profit_acm_d']
+    dup = dup_max - dup_min
 
-    fecha_f_dup = where_max_foll.iloc[0]['timestamp']
-    fecha_i_dup = where_min_foll.iloc[0]['timestamp']
-    drawup = "{}, {}, ${:.2f}".format(fecha_i_dup, fecha_f_dup, dup)
+    date_max_dup = max_foll.iloc[0]['timestamp']
+    date_min_dup = min_foll.iloc[0]['timestamp']
+    drawup = "{}, {}, ${:.2f}".format(date_min_dup, date_max_dup, dup)
+    # Metricas
     metricas = pd.DataFrame({'metrica': ['sharpe', 'drawdown_capi', 'drawdup_capi']})
     valor = pd.DataFrame({'valor': [((rp.mean() - rf) / rp.std()), (drawdown), (drawup)]})
     df_mad1 = pd.merge(metricas, valor, left_index=True, right_index=True)
